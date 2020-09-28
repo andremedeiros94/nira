9d458bb14d488063795813d203a982f51117fd2d
André Medeiros <andre.d.medeiros@ufv.br>
2020-09-28 14:09:24 -0300

    up import convert
    converter de cm-1 pra nm

--- R/import_csv.R
+++ R/import_csv.R
@@ -6,7 +6,7 @@
 #' @import readxl
 #' @export
 #' 
-import_csv<-function(directory=choose.dir()){
+import_csv<-function(directory=choose.dir(),convert=F){
   setwd(directory)
   lista<-list.files(pattern="*.csv",full.names=F)
   arquivos <- lapply(lista, function(x) read.csv(x, header=F)) 
@@ -14,6 +14,10 @@ import_csv<-function(directory=choose.dir()){
   elim<-seq(from = 3, to = length(df), by = 2)
   df<-df[,-c(elim)]
   df<-as.data.frame(t(df))
+  conv=convert
+  if(convert=T){
+    df[1,]<-10000000/df[1,]
+  }
   names(df) <- df[1,]
   df <- df[-1,]
   rownames(df)<-lista
