fc51328a6a91f84e70a27237bb67312e9fdbbe6c
André Medeiros <andre.d.medeiros@ufv.br>
2020-10-06 15:33:46 -0300

    up

--- R/import_csv.R
+++ R/import_csv.R
@@ -6,7 +6,7 @@
 #' @import readxl
 #' @export
 #' 
-import_csv<-function(directory=choose.dir(),convert=F){
+import_csv<-function(directory=choose.dir(),conv=FALSE){
   setwd(directory)
   lista<-list.files(pattern="*.csv",full.names=F)
   arquivos <- lapply(lista, function(x) read.csv(x, header=F)) 
@@ -14,8 +14,8 @@ import_csv<-function(directory=choose.dir(),convert=F){
   elim<-seq(from = 3, to = length(df), by = 2)
   df<-df[,-c(elim)]
   df<-as.data.frame(t(df))
-  conv=convert
-  if(convert=T){
+  convert=conv
+  if(convert==TRUE){
     df[1,]<-10000000/df[1,]
   }
   names(df) <- df[1,]
